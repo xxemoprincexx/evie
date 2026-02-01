1d1624eb56e0d359e8dbdcf8b710b7a976f46ddf
xxemoprincexx <wireless.damienvalphiferus@gmail.com>
2026-02-01 15:17:20 -0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://www.youtube.com/watch?v=qUb1qlLNL9M", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dtuwliexo/video/upload/v1769987781/digi_angel_by_Bonnie_N_Eve_lyric_video_cwvquo.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
